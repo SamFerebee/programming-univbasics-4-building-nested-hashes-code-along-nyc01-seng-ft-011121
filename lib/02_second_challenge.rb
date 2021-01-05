cd850defd154cb6f7596db18871871e2e43513fc
Samuel Ferebee <sam.ferebee@gmail.com>
2021-01-05 17:46:12 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,11 +1,18 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      #your key/value pairs here!
+      :patriarch => {},
+      :matriach => {},
+      :hero => {},
+      :hero_friends => []
       }, 
     :capulet => {
-      #your key/value pairs here!
-      }}
+      :patrich => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+      }
+  }
 
   
 end
